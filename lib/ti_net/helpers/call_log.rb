f8f34f39d0cba13e4f7402ef429e9f5a9eaf3da1
angelfan <angelfan1314@163.com>
2017-05-09 16:51:51 +0800

    fix method not found bug

--- lib/ti_net/helpers/call_log.rb
+++ lib/ti_net/helpers/call_log.rb
@@ -2,7 +2,14 @@ module TiNet
   module Helpers
     module CallLog
       def record_file_url(origin_url)
-        "#{origin_url}?#{default_params.to_query}"
+        seed = SecureRandom.hex
+        params = {
+          enterpriseId: TiNet.config.enterprise_id,
+          user_name: TiNet.config.username,
+          seed: seed,
+          pwd: Digest::MD5.hexdigest("#{Digest::MD5.hexdigest(TiNet.config.pwd)}#{seed}")
+        }
+        "#{origin_url}?#{params.to_query}"
       end
     end
   end
--- lib/ti_net/helpers/vlink.rb
+++ lib/ti_net/helpers/vlink.rb
@@ -2,7 +2,13 @@ module TiNet
   module Helpers
     module Vlink
       def record_file_url(origin_url)
-        "#{TiNet.config.vlink_host}/#{origin_url}?#{default_params.to_query}"
+        timestamp = Time.now.to_i
+        params = {
+          appId: TiNet.config.vlink_app_id,
+          timestamp: timestamp,
+          sign: Digest::MD5.hexdigest("#{TiNet.config.vlink_app_id}#{TiNet.config.vlink_token}#{timestamp}")
+        }
+        "#{TiNet.config.vlink_host}/#{origin_url}?#{params.to_query}"
       end
     end
   end
